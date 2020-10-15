7b4bbbf59e5281431be58651c80ac35a0ca6edaf
Steven-Tang-rong <gzodc0045288@gmail.com>
2020-10-15 19:47:55 +0800

    Initial Commit

---
+++ songPlayerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  songPlayerDemo
+//
+//  Created by TANG,QI-RONG on 2020/10/15.
+//  Copyright © 2020 Steven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
